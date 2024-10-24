73090f337125c7b82f7e23f5a6a45460187eec59
Rodolfo Chacon <rudychacon01@gmail.com>
2024-10-23 20:30:19 -0600

    empezando2

--- App.js
+++ App.js
@@ -1,16 +1,5 @@
 console.log("Bienvenido a Aplicaciones Matemáticas");
-console.log("Ingrese una opcion [1]Serie de Fibonacci  [2]Cálculo del Factorial [3]Verificación de Números Primos");
-
-
-
-
-
-
-
-
-
-
-
+console.log("Ingrese una opcion [1]Serie de Fibonacci  [2]Cálculo del Factorial [3]Verificación de Números Primos [0] para salir");
 
 
 

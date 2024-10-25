79963a3c5588847eec8fc3a4d8d477b3e308cf55
Kevin Chacon <kevin27chacon@gmail.com>
2024-10-25 09:53:31 -0600

    se agregó codigo base de Fibonacci
    el codigo todavia no da la información correcta

--- App.js
+++ App.js
@@ -1,6 +1,22 @@
 console.log("Bienvenido a Aplicaciones Matemáticas");
 console.log("Ingrese una opcion [1]Serie de Fibonacci  [2]Cálculo del Factorial [3]Verificación de Números Primos [0] para salir");
 
+function Fibonacci(n) {
+    if (n>1000) {
+        return;
+    }
+
+    if(n<=2){
+        console.log(n)
+        return Fibonacci(n + 1)
+    } else{
+        return Fibonacci(n-1) + Fibonacci(n-2)
+    }
+
+};
+
+console.log(Fibonacci(0))
+
 
 
 

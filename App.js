aa0a7fbcd9a7d40bce2e9a9eb2e4efe066a7795d
Esteph11 <esteportilloluna11@gmail.com>
2024-10-27 17:34:10 -0600

    CalculoFactorial
    Aplicaciones Matemáticas en Node.js

--- App.js
+++ App.js
@@ -15,20 +15,8 @@ console.log();
 // main
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-//funciones
-
+//Funciones
+//Función Fibonacci 
 function Fibonacci(n) {
     if (n===0){
         return 0;
@@ -45,9 +33,18 @@ for (let f=0; f<25; f++) {
 }
 
 
+// Cálculo del Factorial  
+function factorial(n) {  
+    if (n < 0) {  
+        return "No se puede calcular el factorial de un número negativo.";  
+    } else if (n === 0 || n === 1) {  
+        return 1;  
+    } else {  
+        return n * factorial(n - 1);  
+    }  
+} 
 
-
-
+// Números Primos  
 function primo(n){
 
 if(n>=20) 
@@ -71,7 +68,54 @@ primo(n+1);
 
  }
     
+// Función qe menú  
+ function Menu(opcion) {  
+    switch (opcion) {  
+        case '1':  
+            console.log("Serie de Fibonacci:");  
+            for (let f = 0; f < 25; f++) {  
+                console.log(Fibonacci(f));  
+            }  
+            break;  
+        case '2':  
+        readline.question('Ingrese un número para calcular su factorial: ', num => {  
+            const numeroFactorial = parseInt(num);  
+            console.log(`El factorial de ${numeroFactorial} es: ${factorial(numeroFactorial)}`);  
+            Opciones(); // Vuelve a solicitar las opciones
+        });  
+        return;  
+        case '3':  
+            readline.question('Ingrese un número para verificar si es primo: ', num => {  
+                const numeroPrimo = parseInt(num);  
+                console.log(`${numeroPrimo} es primo: ${primo(numeroPrimo)}`);  
+                Opciones(); 
+            });  
+            return; 
+        case '0':  
+            console.log("Cerrando el programa...");  
+            process.exit(); // Termina de ejecutar el programa  
+            break;  
+        default:  
+            console.log("Opción no válida, ingrese una opción correcta.");  
+            break;  
+    }  
+}  
 
+// Ejecución del programa  
+const readline = require('readline').createInterface({  
+    input: process.stdin,  
+    output: process.stdout  
+});  
+
+function Opciones() {  
+    readline.question('Ingrese su opción: ', opcion => {  
+        Menu(opcion);  
+        Opciones(); //Solicita nuevamente las opciones
+    });  
+}  
+
+// Inicio de las opciones  
+Opciones();  
 
 
 
